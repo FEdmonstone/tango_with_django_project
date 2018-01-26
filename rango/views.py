7fadc42c2fb34c26c05a1c402b4077945b0eeeef
Kirsten <2247435e@student.gla.ac.uk>
2018-01-26 15:33:50 +0000

    chapter 6 v3

--- rango/views.py
+++ rango/views.py
@@ -6,13 +6,16 @@ from rango.models import Page
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories':category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories':category_list,
+                    'pages':page_list}
     
     return render(request, 'rango/index.html', context_dict)
 
 def show_category(request, category_name_slug):
 
     context_dict = {}
+    
     try:
 
         category = Category.objects.get(slug=category_name_slug)
@@ -24,6 +27,7 @@ def show_category(request, category_name_slug):
         context_dict['category'] = category
         
     except Category.DoesNotExist:
+        
         context_dict['category'] = None
         context_dict['pages'] = None
         
